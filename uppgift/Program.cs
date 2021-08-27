7347d864a07a0d03ea8321129179817e5f6a2778
HibbaHindawi <hibba04@hotmail.se>
2021-08-27 14:06:15 +0200

    made robot

---
+++ uppgift/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Är det fint väder? Y/N");
+            string weather= Console.ReadLine();
+            if (weather == "Y" || weather == "y")
+            {
+                Console.WriteLine("Vi går på picknick!");}
+            else if (weather == "N" || weather == "n")
+            {
+                Console.WriteLine("Vi stannar inne och läser en bok.");
+            }
+            else
+            {
+                Console.WriteLine("Jag förstår inte!");
+            }
+            Console.Read();
+        }
+    }
+}
